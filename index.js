3dab87bb88db317993d567953f98708c9f3721d9
Grant Timmerman <granttimmerman@gmail.com>
2017-02-13 14:18:05 -0800

    Fix prediction

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
+var product = require('cartesian-product');
 var TrieSearch = require('trie-search');
 
 // t9
 var T9Search = function() {};
+var numbers = {
+  2: ['a', 'b', 'c'],
+  3: ['d', 'e', 'f'],
+  4: ['g', 'h', 'i'],
+  5: ['j', 'k', 'l'],
+  6: ['m', 'n', 'o'],
+  7: ['p','q', 'r', 's'],
+  8: ['t', 'u', 'v'],
+  9: ['w', 'x', 'y', 'z']
+}
 
 /**
  * Sets an array of words to the dictionary.
@@ -18,13 +29,18 @@ T9Search.prototype.setWords = (words) => {
 
 /**
  * A prefix to the predicted word
- * @param  {String} prefix The string prefix
+ * @param  {String} prefix The number. '228'
  * @return {String[]} A list of guessed words
  */
 T9Search.prototype.predict = (prefix) => {
-  var predictions = this.trie.get(prefix, {
-    min: 5
-  });
+  // 1. Convert numbers to arrays of values
+  var letters = prefix.split('').map(c => {
+    return numbers[c];
+  })
+  var combos = product(letters);
+  var predictions = combos.map(combo => combo.join(''))
+    .map(prefix => this.trie.get(prefix, { min: 5 })
+    .filter(a => a.length)[0];
   return predictions.map(prediction => prediction.value);
 };
 
--- test.js
+++ test.js
@@ -3,5 +3,5 @@ var words = ['cat', 'bot', 'catapult', 'foo'];
 var t9 = new T9Search();
 t9.setWords(words);
 
-var prediction = t9.predict('c');
-console.log(prediction); // ['ca', 'catapult']
+var prediction = t9.predict('22');
+console.log(prediction); // ['cat', 'catapult']
